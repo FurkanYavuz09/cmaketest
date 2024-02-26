9c9895f8ea9b83039d5b89438b71ef3767221fd6
Furkan Yavuz <furkanyavuz0926@gmail.com>
2024-02-26 15:44:47 +0300

    initial commit

---
+++ build/CMakeFiles/cmakedeneme.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmakedeneme.dir/main.cpp.o"
+  "CMakeFiles/cmakedeneme.dir/main.cpp.o.d"
+  "cmakedeneme"
+  "cmakedeneme.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmakedeneme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
